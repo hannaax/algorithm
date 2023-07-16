f64519c9b6fe273288358c6977e738c43d762fbf
hannaax <111215320+hannaax@users.noreply.github.com>
2023-07-16 16:51:21 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,16 @@
+function solution(food) {
+    var answer = '';
+    for(let i=1; i<food.length; i++){
+        answer += String(i).repeat(Math.floor(food[i]/2))
+    }
+    answer += "0"
+    answer = answer + answer.split('').reverse().join('').slice(1) 
+    return answer;
+}
+
+// (음식배치)물(음식배치reverse)니까
+// (음식배치)물 먼저 한 후에, 음식배치reverse 더하기
+
+// 두선수의 음식 종류, 양이 같음
+// 칼로리가 낮은 음식 먼저 배치
+// 조건x 음식은 대회에 사용x
