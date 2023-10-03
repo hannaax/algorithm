cb53e9d23cda43d5fe55092749b474d92bcfeb6e
hannaax <111215320+hannaax@users.noreply.github.com>
2023-10-03 09:11:13 +0900

    [level 2] Title: [1차] 캐시, Time: 26.42 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,22 +1,35 @@
 function solution(cacheSize, cities) {
-  var answer = 0;
-  let cache = [];
-  //캐시 크기가 0인 경우는 따로 처리
-  if (cacheSize === 0) return cities.length * 5;
-
-  while (cities.length) {
-    const city = cities.shift().toLowerCase();
-    if (cache.includes(city)) {
-      cache.splice(cache.indexOf(city), 1);
-      cache.push(city);
-      answer += 1;
-    } else {
-      if (cache.length === cacheSize) {
-        cache.shift();
-      }
-      cache.push(city);
-      answer += 5;
-    }
-  }
-  return answer;
+    var answer = 0;
+    let cache = Array.from({length: cacheSize}, ()=>null)
+    
+    console.log(cache)
+    
+    if(cacheSize === 0) return cities.length*5
+    
+    cities.forEach((city) => {
+        city = city.toUpperCase()
+            if(cache.includes(city)){
+                if(cache[0]===city) answer++;
+                else{
+                    let idx = cache.indexOf(city);
+                    for(let i=idx; i>=0; i--){
+                        cache[i] = cache[i-1]
+                    }
+                    cache[0] = city;
+                    answer+=1;
+                }
+            }
+            else{
+                for(let j=cache.length-1; j>=0; j--){
+                    cache[j] = cache[j-1]
+                }
+                cache[0] = city;
+                answer+=5;
+            }
+    })
+    
+    return answer;
 }
+
+
+// 소요 시간: 문제이해-15분, 문제풀이-40분
