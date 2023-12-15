03886931634251f2f6c8b097d51f4ff76dd398ce
hannaax <111215320+hannaax@users.noreply.github.com>
2023-12-15 09:32:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    let answer = 0;
+    while(num !== 1 && answer<=500){
+        if(num%2 === 0) {
+            num = num/2
+            answer++;
+        }
+        else {
+            num = (num*3)+1
+            answer++;
+        }
+    }
+    if(num > 1 && answer>500) answer = -1;
+    return answer;
+}
