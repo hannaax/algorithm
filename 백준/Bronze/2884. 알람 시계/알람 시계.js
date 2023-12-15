8cf98b7fe3874b6d1b9ec2e798619036dad0bc94
hannaax <111215320+hannaax@users.noreply.github.com>
2023-12-15 12:16:11 +0900

    [Bronze III] Title: 알람 시계, Time: 152 ms, Memory: 9460 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -8,18 +8,12 @@ let input = [];
 readline.on('line', function(line) {
     input = line.split(' ').map(el => parseInt(el));
 }).on('close', function(){
-    let hour = input[0];
-    let minute = input[1] - 45;
-
-    if (minute < 0) {
-        minute += 60;
-        hour--;
-
-        if (hour < 0) {
-            hour = 23;
-        }
-    }
-
-    console.log(hour, minute);
+    const hour = input[0]
+    const minute = input[1]
+    
+    if(hour===0 && minute<45) console.log(23, 60-(45-minute))
+    else if(minute<45) console.log(hour-1, 60-(45-minute))
+    else console.log(hour, minute-45)
+    
     process.exit();
 });
