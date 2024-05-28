3156d5960d5e5b67f32b2018ca8f8ee750d0c62e
hannaax <111215320+hannaax@users.noreply.github.com>
2024-05-28 21:35:13 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    commands.forEach((command) => {
+    answer.push(array.slice(command[0]-1,command[1]).sort((a,b)=>a-b)[command[2]-1])
+})
+    
+    return answer;
+}
